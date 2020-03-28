bc2ab7405094161d5d5e77b70335222535bc5dd2
Ethan Dalool <ethan@voussoir.net>
2020-03-27 23:26:22 -0700

    Fix link to new css location.

--- voussoir.net/writing/generate_site.py
+++ voussoir.net/writing/generate_site.py
@@ -279,7 +279,7 @@ def maketagpage(index, path):
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="/writing/css/dark.css"/>
+    <link rel="stylesheet" href="/writing/dark.css"/>
     <style>
     body
     {
@@ -359,7 +359,7 @@ def write_writing_index():
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="/writing/css/dark.css"/>
+    <link rel="stylesheet" href="/writing/dark.css"/>
     </head>
 
     <body>
