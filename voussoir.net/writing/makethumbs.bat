1e0bd6e566fa1ec1483fb919d4bdbdd55dc9c08e
Ethan Dalool <git@voussoir.net>
2024-07-30 23:20:15 -0700

    Step up article photos to 1440 and 80 quality.

--- voussoir.net/writing/makethumbs.bat
+++ voussoir.net/writing/makethumbs.bat
@@ -1,6 +1,6 @@
 rem A width of 905 makes the image occupy the entire horizontal width of the
 rem article, at least with the current font settings.
 md thumbs
-resize *.jpg --width 905 --only-shrink --output .\thumbs\{filename} --quality 50
-resize *.png --width 905 --only-shrink --output .\thumbs\{filename} --quality 50
+resize *.jpg --width 1440 --only-shrink --output .\thumbs\{filename} --quality 80
+resize *.png --width 1440 --only-shrink --output .\thumbs\{filename} --quality 80
 optipng thumbs\*.png
